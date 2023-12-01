f676f482882140ccd28cb8d02590035b06be6980
Stephan Noyvirt <noyvirtstephan@gmail.com>
2023-12-01 12:35:24 +0000

    p2 done

--- day1/main.py
+++ day1/main.py
@@ -1,8 +1,15 @@
 from pathlib import Path
 totalValue = 0
+fakeNumbers = ["one", "two", "three","four","five","six","seven","eight","nine"]
 p = Path(__file__).with_name('input.txt') 
 with p.open('r') as f:
     for line in f:
+        print("line:",line)
+        for index, fakeNumber in enumerate(fakeNumbers):
+            if fakeNumber in line:
+                replacement = str(fakeNumber[0] + str(index+1) + str(fakeNumber[-1]))
+                line=line.replace(fakeNumber, replacement)
+        print("newLine:", line)
         currentDigit=''
         for character in line:
             if character.isdigit():
@@ -13,5 +20,7 @@ with p.open('r') as f:
             if character.isdigit():
                 currentDigit+=(character)
                 break
+        print(currentDigit)
+        print()
         totalValue += int(currentDigit)
 print(totalValue)
